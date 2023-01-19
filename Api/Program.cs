90e5da66af8cae93d923042bec9d162d6090c89b
Jefferson <renemarin96jc@gmail.com>
2023-01-19 09:20:35 -0500

    Configuración de la aplicación para el perfil de automapper, dbcontext, patrón repositorio, JWT, cors y filtros

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,25 +1,84 @@
+using BL.Common;
+using BL.Models;
+using BL.Repositories.Implements;
+using BL.Repositories;
+using BL.Services.Implements;
+using BL.Services;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+using Api.Filters;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//Variables de configuración
+var configuration = builder.Configuration;
+var appSettingsSection = configuration.GetSection("AppSettings");
+var appSettings = appSettingsSection.Get<AppSettings>();
+var key = Encoding.ASCII.GetBytes(appSettings.SecretKey);
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//Configuración para la inyección del perfil de automapper
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+//Configuración del cors
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "ConfigCors", builder =>
+    {
+        builder.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost")
+        .AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
+//Configuración para agregar el dbcontext, repositorios y servicios
+builder.Services.AddDbContext<DbSuscripcionEventosContext>(options => options.UseSqlServer(configuration.GetConnectionString("connectionString")));
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+builder.Services.AddScoped<IUsuarioService, UsuarioService>();
+builder.Services.Configure<AppSettings>(appSettingsSection);
+
+//Configuración para el filtro de entidades no procesadas y deshabilitar el comportamiento 400 automático
+builder.Services.AddScoped<ValidationFilterAttribute>();
+builder.Services.Configure<ApiBehaviorOptions>(options
+    => options.SuppressModelStateInvalidFilter = true);
+
+//Configuracion de la autenticación basada en JWT
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(z =>
+{
+    z.RequireHttpsMetadata = false;
+    z.SaveToken = true;
+    z.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    //Configuración para las propiedades que inicien con letra mayúscula
+    options.JsonSerializerOptions.PropertyNamingPolicy = null;
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors("ConfigCors");
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
