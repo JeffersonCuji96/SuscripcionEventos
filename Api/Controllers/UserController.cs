c6aba4f00f01f4d6edd913980685fff4bb78e0f8
Jefferson <renemarin96jc@gmail.com>
2023-01-30 17:54:43 -0500

    Se agrega controlador con el método para registrar un usuario y helper con métodos  para subir y remover archivos de la ruta del servidor

---
+++ Api/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using Api.Filters;
+using Api.Helpers;
+using AutoMapper;
+using BL.DTO;
+using BL.Models;
+using BL.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IMapper mapper;
+        private readonly IUsuarioService userService;
+        private readonly IWebHostEnvironment hostingEnviroment;
+        public UserController(
+            IMapper _mapper, 
+            IUsuarioService userService,
+            IWebHostEnvironment hostingEnviroment)
+        {
+            mapper = _mapper;
+            this.userService = userService;
+            this.hostingEnviroment = hostingEnviroment;
+        }
+
+        [HttpPost]
+        [ServiceFilter(typeof(ValidationFilterAttribute))]
+        public IActionResult Save(UsuarioDTO usuarioDTO)
+        {
+            var oUsuario = mapper.Map<Usuario>(usuarioDTO);
+            if (!string.IsNullOrEmpty(usuarioDTO.ImageBase64))
+                oUsuario.Persona.Foto = hostingEnviroment.ContentRootPath + FileHelper.UploadImage(usuarioDTO.ImageBase64);
+            userService.InsertUserPerson(oUsuario);
+            return Ok(new { Message = "Usuario registrado con éxito!" });
+        }
+    }
+}
---
+++ Api/Helpers/FileHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace Api.Helpers
+{
+    public class FileHelper
+    {
+        public static string UploadImage(string imageBase64)
+        {
+            var nameFile = Guid.NewGuid().ToString();
+            var stream = new MemoryStream(Convert.FromBase64String(imageBase64));
+            IFormFile file = new FormFile(stream, 0, stream.Length, nameFile, nameFile);
+            var ruta = $"Resources/Images/{nameFile}" + ".png";
+            using var fileStream = new FileStream(ruta, FileMode.Create);
+            file.CopyTo(fileStream);
+            return ruta;
+        }
+        public static void RemoveImage(string path)
+        {
+            if (!string.IsNullOrEmpty(path))
+                File.Delete(path);
+        }
+    }
+}
